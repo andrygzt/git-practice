2c2bf63be044c53bdd2e50bb0bea40296814f03c
Andrea Garcia <andrygzt@gmail.com>
2022-03-25 14:41:54 -0700

    the one when I re write function d

--- function_d.py
+++ function_d.py
@@ -1,9 +1,9 @@
 def max_value(numbers):
-    """ This function returns the largest number
-        in the list.
-    """
-    pass
-
-
+    max = number[0]
+    for num = numbers:
+        if num > max:
+            max = num
+    return max
+    
 if __name__ == "__main__":
     print(max_value([1, 12, 2, 42, 8, 3]))
